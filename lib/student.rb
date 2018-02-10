9c15ebfafbcd5e5d44bb3eb1d81c3a912e94445f
Rauof M <abdelrauof197@gmail.com>
2018-02-10 04:58:08 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,7 +58,7 @@ class Student
     sql = <<-SQL
     SELECT * FROM students WHERE grade = 10 LIMIT 1
     SQL
-    DB[:conn].execute(sql,x).map { |row|
+    DB[:conn].execute(sql).map { |row|
       new_from_db(row) 
     }
   end
