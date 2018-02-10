77a8d6873fb0fc8f6dcef031604238c0ab09b455
Rauof M <abdelrauof197@gmail.com>
2018-02-10 04:53:36 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,7 +47,7 @@ class Student
   
   def first_X_students_in_grade_10(x)
     sql = <<-SQL
-    SELECT * FROM students WHERE grade > 10 LIMIT ?
+    SELECT * FROM students WHERE grade = 10 LIMIT ?
     SQL
     
     DB[:conn].execute(sql,x).map { |row|
